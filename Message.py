6608ab82391699d50a71c8b95e0293584527705a
vovans32 <47394508+vovans32@users.noreply.github.com>
2019-10-22 16:16:51 +0500

    123
    321

---
+++ Message.py
@@ -0,0 +1,25 @@
+from PyQt5.QtWidgets import QLabel
+from time import asctime
+
+
+class Message:
+    def __init__(self, text, sender='Вы'):
+        self.sender = sender
+        self.time = asctime()
+        self.text = (text + '\n')
+
+    def text_to_show(self):
+        text = f'{self.sender} ({self.time}):\n{self.text}'
+        label = QLabel(text)
+        # if self.sender == 'Вы':
+        #     label.setStyleSheet('font-size: 25px; color: black')
+        # else:
+        label.setStyleSheet('''
+        font-size: 25px;
+        color: black;
+        background-color: #BED6FF;
+        border-style: outset;
+        border-width: 2px;
+        border-radius: 10px;
+        border-color: white''')
+        return label
