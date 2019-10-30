70b5cfa83771bbb803c8efd279e043b5b87f7edb
Timur <hts1238@mail.ru>
2019-10-30 11:31:10 +0500

    test gg

--- backend/login.php
+++ backend/login.php
@@ -1,42 +1,42 @@
-<?php // login.php
+<?php // login.php ggg
 
 include_once("functions.php");
 
 header('Content-Type: application/json');
 
 function login(&$query_answer) {
-	if (!isset($_POST["handle"]) || !isset($_POST["password"])) {
-		$query_answer["Error"] = "There are not all post-parameters";
-		return;
-	}
+  if (!isset($_POST["handle"]) || !isset($_POST["password"])) {
+    $query_answer["Error"] = "There are not all post-parameters";
+    return;
+  }
 
     $handle = $_POST["handle"];
     $password = $_POST["password"];
-	
-	$db = connect ();
-	if (!$db) {
-		$query_answer["Error"] = "Couldn't connect to database";
-		return;
-	}
-	
-	$sql = "SELECT id ".
+  
+  $db = connect ();
+  if (!$db) {
+    $query_answer["Error"] = "Couldn't connect to database";
+    return;
+  }
+  
+  $sql = "SELECT id ".
         "FROM users ".
-		"WHERE handle = '$handle'";
-	$sql_result = mysqli_query($db, $sql);
+    "WHERE handle = '$handle'";
+  $sql_result = mysqli_query($db, $sql);
 
     if (!$sql_result) {
         $query_answer["Error"] = "Database error while checking handle";
         return;
     }
-	
-	if (mysqli_fetch_assoc($sql_result) != null) {
-	    $query_answer["Error"] = "The user with this handle not exist";
-	    return;
+  
+  if (mysqli_fetch_assoc($sql_result) != null) {
+      $query_answer["Error"] = "The user with this handle not exist";
+      return;
     }
     
     $user_id = $sql_result["id"];
 
-	$new_token = create_token();
+  $new_token = create_token();
 
     $sql = "SELECT * ".
         "FROM connects ".
